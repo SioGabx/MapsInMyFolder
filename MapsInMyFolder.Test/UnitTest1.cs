339a4f27cb75dbd42467a4ea90a1024900854756
SioGabx <contact@siogabx.fr>
2024-05-30 11:49:18 +0200

    Added UnitTest project

---
+++ MapsInMyFolder.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace MapsInMyFolder.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
